b8a8206a1c19716ffce852d920957cae437c6aa0
Mao <37288931+mao0507@users.noreply.github.com>
2022-12-28 00:40:07 +0800

    fix: Delete `␍`  bug

--- src/views/undefinedPage.vue
+++ src/views/undefinedPage.vue
@@ -5,10 +5,4 @@
             v-col.d-flex.align-center.justify-center(col="12" style="height:100vh")
                 v-sheet.d-flex.align-center.justify-center(rounded="lg"  height="360" width="480")
                     h1.blue-grey--text(style="font-size:6em") 404
-                  
-                        
-
-       
-
-
 </template>
