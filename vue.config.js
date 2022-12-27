d499685ed0642606881828646ed77eb670ad4a44
Mao <37288931+mao0507@users.noreply.github.com>
2022-12-27 23:22:30 +0800

    set path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   publicPath:
-    process.env.NODE_ENV === "production" ? "/git_message/" : "/git_message",
+    process.env.NODE_ENV === "production"
+      ? "/open_a_position/"
+      : "/open_a_position",
   transpileDependencies: ["vuetify"],
 });
