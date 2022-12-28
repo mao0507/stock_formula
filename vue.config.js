963efe689242d6b59f6079873f59930dbef0ce59
Mao <37288931+mao0507@users.noreply.github.com>
2022-12-28 23:06:04 +0800

    feat:調整 publicPath

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   publicPath:
     process.env.NODE_ENV === "production"
-      ? "/open_a_position/"
-      : "/open_a_position",
+      ? "/stock_formula/"
+      : "/stock_formula",
   transpileDependencies: ["vuetify"],
 });
