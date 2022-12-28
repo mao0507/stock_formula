a6c5c5d057ee57dc012235a88ce8b6dbf36f3ad3
Mao <37288931+mao0507@users.noreply.github.com>
2022-12-28 23:01:30 +0800

    feat:增加filter 千分位

--- src/main.js
+++ src/main.js
@@ -6,9 +6,21 @@ import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
 
+Vue.filter("thousandComma", (num) => {
+  if (num == null || num == undefined || num == "" || isNaN(num)) {
+    //console.log("缺少num參數");
+    return 0;
+  }
+  var str = num.toString();
+  var reg =
+    str.indexOf(".") > -1 ? /(\d)(?=(\d{3})+\.)/g : /(\d)(?=(?:\d{3})+$)/g;
+  return str.replace(reg, "$1,");
+});
+
 new Vue({
   router,
   store,
   vuetify,
+
   render: (h) => h(App),
 }).$mount("#app");
