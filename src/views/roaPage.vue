1684898d401b8edaea51b925e900cc50b649ed55
Mao <37288931+mao0507@users.noreply.github.com>
2022-12-29 00:36:11 +0800

    feat: 新增roa

---
+++ src/views/roaPage.vue
@@ -0,0 +1,70 @@
+<template lang="pug">
+  v-col(cols="12" sm="8")
+    v-overlay(:value="overlay")
+      v-progress-circular(indeterminate size="64")
+    v-card(min-height="80vh" rounded="lg" :dark="isDark")
+      v-card-title 
+        v-icon mdi-apps-box
+        span.pl-2 資產報酬率 (ROA) 計算
+      v-card-text
+        v-container
+          v-text-field.my-3(v-bind="textSetting" label="稅後淨利" v-model="calculate.netProfitAfterTax") 
+          v-text-field.my-3(v-bind="textSetting" label="總資產" v-model="calculate.totalAssets") 
+          
+
+          v-divider.mb-2
+
+          h3 稅後淨利 ： {{ calculate.netProfitAfterTax | thousandComma}}
+          br
+          h3 平均總資產 ： {{calculate.totalAssets | thousandComma}}
+          br
+          h3 資產報酬率 ： {{ROA + ' %' }}  
+        
+      
+</template>
+<script>
+export default {
+  data: () => ({
+    isDark: null,
+    overlay: true,
+    calculate: {
+      netProfitAfterTax: null,
+      totalAssets: null,
+    },
+
+    textSetting: {
+      outlined: true,
+      "hide-details": true,
+      dense: true,
+      type: "number",
+      min: 0,
+    },
+  }),
+  computed: {
+    ROA() {
+      if (
+        this.calculate.netProfitAfterTax == null ||
+        this.calculate.totalAssets == null
+      )
+        return 0;
+      else
+        return (
+          (this.calculate.netProfitAfterTax / this.calculate.totalAssets) * 100
+        );
+    },
+  },
+  methods: {},
+  mounted() {
+    setTimeout(() => {
+      this.overlay = false;
+    }, 500);
+    this.isDark = this.$store.state.dark;
+    console.log("isDark", this.isDark);
+  },
+  watch: {
+    "$store.state.dark"(newValue) {
+      this.isDark = newValue;
+    },
+  },
+};
+</script>
